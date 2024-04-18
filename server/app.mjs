108103756ffe428d31995aa5538ff1bfea9b40f4
napatwongchr <napat.wongchr@gmail.com>
2024-04-18 09:15:46 +0700

    feat: add create assignment mock api

--- server/app.mjs
+++ server/app.mjs
@@ -3,8 +3,16 @@ import express from "express";
 const app = express();
 const port = 4001;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.post("/assignments", (req, res) => {
+  // สมมุติว่าตรงนี้มีโค้ดที่เขียน Query เชื่อมต่อกับฐานข้อมูล
+  // เพื่อสร้าง Assignment เขียนเอาไว้แล้ว
+  return res.status(201).json({ message: "Create assignment successfully" });
 });
 
 app.listen(port, () => {
